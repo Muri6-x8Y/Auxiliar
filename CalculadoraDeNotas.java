536b016f33c412aa5969f13847293877bb45257b
Muri6-x8Y <muriloesamul@gmail.com>
2025-10-25 13:09:12 -0300

    corrigindo lógica de arrayIndexOutOfBounds

--- CalculadoraDeNotas.java
+++ CalculadoraDeNotas.java
@@ -8,7 +8,7 @@ public class CalculadoraDeNotas {
 
     public double calcularMedia() {
         double soma = 0;
-        for (int i = 0; i <= notas.length; i++) {
+        for (int i = 0; i < notas.length; i++) {
             soma += notas[i];
         }
         return soma / notas.length;
